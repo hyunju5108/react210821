9bec2252b5b2e64ed5e7df6696498117976f1799
나현주 <hyunju@nahyeonjuui-MacBookPro.local>
2021-09-04 10:40:18 +0900

    header style 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
-import TodoPage from './pages/todo'
+import TodoPage from './pages/Todo'
 import ErrorPage from './pages/Error'
 import ResetStyles from './components/ResetStyles'
 
@@ -9,22 +9,24 @@ const ROUTES = {
   TODO: '/',
   SIGNUP: '/signup',
 }
-/*
- * 컴포넌트들의 최상위 컴포넌트
- */
 
+/**
+ * 컴포넌트들의 최상위 컴포넌트
+ *
+ * <div />
+ * <div />
+ */
 function App() {
   return (
     <>
       <ResetStyles />
       <BrowserRouter>
-        {/* 안바꼈으면 좋겠는 부분은 switch밖에다가 쓰면 됨. switch는 계속 바뀌는 영역 */}
         <Switch>
-          {/* /나 /todo로 들어왔을때 todo페이지로 이동 */}
+          {/* / 나 /todo 모두 / */}
           <Route exact path={ROUTES.TODO}>
             <TodoPage />
           </Route>
-          {/* 그 외에 정의되지 않은 페이지는 error page */}
+          {/* 그 외에 정의되지 않은 페이지는 error page  */}
           <Route>
             <ErrorPage />
           </Route>
